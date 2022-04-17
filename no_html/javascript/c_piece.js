2307274b2344550c32b39125da9967986006c4dc
deukay <dk58554@gmail.com>
2022-04-17 18:15:52 +0300

    added moveset view to each piece + compacted some code

--- no_html/javascript/c_piece.js
+++ no_html/javascript/c_piece.js
@@ -1,8 +1,10 @@
 class c_piece {
-    constructor(x, y, p_type) {
+    constructor(x, y, p_type, isWhite, isfirstMove) {
         this.x = x;
         this.y = y;
         this.p_type = p_type;
+        this.isWhite = isWhite;
+        this.isfirstMove = isfirstMove;
     }
 
     getArrPos() {
@@ -10,36 +12,225 @@ class c_piece {
     }
 
     getImg() {
+        let output = "";
         switch (this.p_type) {
-            //white pieces 0-5
+            //pieces type 0-5
             case 0:
-                return "kw";
+                output = "k";
+                break;
             case 1:
-                return "qw";
+                output =  "q";
+                break;
             case 2:
-                return "rw";
+                output =  "r";
+                break;
             case 3:
-                return "bw";
+                output =  "b";
+                break;
             case 4:
-                return "nw";
+                output =  "n";
+                break;
             case 5:
-                return "pw";
-            //black pieces 5-11
-            case 6:
-                return "kb";
-            case 7:
-                return "qb";
-            case 8:
-                return "rb";
-            case 9:
-                return "bb";
-            case 10:
-                return "nb";
-            case 11:
-                return "pb";
+                output =  "p";
+                break;
             default:
                 console.log("p_type out of bounds");
                 break;
         }
+        if(this.isWhite) {
+            return output + "w";
+        } else {
+            return output + "b";
+        }
+    }
+
+    possibleMoves() {
+        let movesPos = [];
+        switch (this.p_type) {
+            //black pieces 0-5
+            case 0: //King moveset
+                if(this.x < 7) {//right
+                    movesPos.push(this.x + 1);
+                    movesPos.push(this.y);
+                    if(this.y < 7) {//down right
+                        movesPos.push(this.x + 1);
+                        movesPos.push(this.y + 1);
+                    }
+                    if(this.y > 0) {//top right
+                        movesPos.push(this.x + 1);
+                        movesPos.push(this.y - 1);
+                    }
+                }
+                if(this.x > 0) {//left
+                    movesPos.push(this.x - 1);
+                    movesPos.push(this.y);
+                    if(this.y < 7) {//down left
+                        movesPos.push(this.x - 1);
+                        movesPos.push(this.y + 1);
+                    }
+                    if(this.y > 0) {//top left
+                        movesPos.push(this.x - 1);
+                        movesPos.push(this.y - 1);
+                    }
+                }
+                if(this.y < 7) {//down
+                    movesPos.push(this.x);
+                    movesPos.push(this.y + 1);
+                }
+                if(this.y > 0) {//up
+                    movesPos.push(this.x);
+                    movesPos.push(this.y - 1);
+                }
+                break;
+            case 1: //Queen = Rook + Bishop movesets
+                //TODO: maybe change the queen switch so it will call the func again instead
+                //Rook move set
+                if(this.x < 7) { // +x (right side)
+                    for (let i = 1; i <= 7 - this.x; i++) {
+                        movesPos.push(this.x + i);
+                        movesPos.push(this.y);
+                    }
+                }
+                if(this.x > 0) {// -x (left side)
+                    for (let i = 1; i <= this.x; i++) {
+                        movesPos.push(this.x - i);
+                        movesPos.push(this.y);
+                    }
+                }
+                if(this.y < 7) { // +y (right side)
+                    for (let i = 1; i <= 7 - this.y; i++) {
+                        movesPos.push(this.x);
+                        movesPos.push(this.y + i);
+                    }
+                }
+                if(this.y > 0) {// -y (left side)
+                    for (let i = 1; i <= this.y; i++) {
+                        movesPos.push(this.x);
+                        movesPos.push(this.y - i);
+                    }
+                }
+                //Bishop moveset
+                if(this.y < 7 && this.x < 7) {
+                    for (let i = 1; i < Math.min(8 - this.x, 8 - this.y); i++) { // +x +y (diag down right)
+                        movesPos.push(this.x + i);
+                        movesPos.push(this.y + i);
+                    }
+                }
+                if(this.y < 7 && this.x > 0) {
+                    for (let i = 1; i <= Math.min(this.x, 8 - (this.y+1)); i++) { // -x -y (diag down left)
+                        movesPos.push(this.x - i);
+                        movesPos.push(this.y + i);
+                    }
+                }
+                if(this.y > 0 && this.x > 0) {
+                    for (let i = 1; i <= Math.min(this.x, this.y); i++) { // -x -y (diag up left)
+                        movesPos.push(this.x - i);
+                        movesPos.push(this.y - i);
+                    }
+                }
+                if(this.y > 0 && this.x < 7) {
+                    for (let i = 1; i < Math.min(8 - this.x, this.y+1); i++) { // -x -y (diag up right)
+                        movesPos.push(this.x + i);
+                        movesPos.push(this.y - i);
+                    }
+                }
+                break;
+            case 2: //Rook moveset
+                if(this.x < 7) { // +x (right side)
+                    for (let i = 1; i <= 7 - this.x; i++) {
+                        movesPos.push(this.x + i);
+                        movesPos.push(this.y);
+                    }
+                }
+                if(this.x > 0) {// -x (left side)
+                    for (let i = 1; i <= this.x; i++) {
+                        movesPos.push(this.x - i);
+                        movesPos.push(this.y);
+                    }
+                }
+                if(this.y < 7) { // +y (right side)
+                    for (let i = 1; i <= 7 - this.y; i++) {
+                        movesPos.push(this.x);
+                        movesPos.push(this.y + i);
+                    }
+                }
+                if(this.y > 0) {// -y (left side)
+                    for (let i = 1; i <= this.y; i++) {
+                        movesPos.push(this.x);
+                        movesPos.push(this.y - i);
+                    }
+                }
+                break
+            case 3: //Bishop moveset
+                if(this.y < 7 && this.x < 7) {
+                    for (let i = 1; i < Math.min(8 - this.x, 8 - this.y); i++) { // +x +y (diag down right)
+                        movesPos.push(this.x + i);
+                        movesPos.push(this.y + i);
+                    }
+                }
+                if(this.y < 7 && this.x > 0) {
+                    for (let i = 1; i <= Math.min(this.x, 8 - (this.y+1)); i++) { // -x -y (diag down left)
+                        movesPos.push(this.x - i);
+                        movesPos.push(this.y + i);
+                    }
+                }
+                if(this.y > 0 && this.x > 0) {
+                    for (let i = 1; i <= Math.min(this.x, this.y); i++) { // -x -y (diag up left)
+                        movesPos.push(this.x - i);
+                        movesPos.push(this.y - i);
+                    }
+                }
+                if(this.y > 0 && this.x < 7) {
+                    for (let i = 1; i < Math.min(8 - this.x, this.y+1); i++) { // -x -y (diag up right)
+                        movesPos.push(this.x + i);
+                        movesPos.push(this.y - i);
+                    }
+                }
+                break;
+            case 4: //Knight moveset
+                if(this.x - 1 >= 0 && this.y - 2 >= 0)
+                {
+                    movesPos.push(this.x - 1);
+                    movesPos.push(this.y - 2);
+                }
+                if(this.x + 1 <= 8 && this.y - 2 >= 0)
+                {
+                    movesPos.push(this.x + 1);
+                    movesPos.push(this.y - 2);
+                }
+                if(this.x - 1 >= 0 && this.y + 2 <= 8)
+                {
+                    movesPos.push(this.x - 1);
+                    movesPos.push(this.y + 2);
+                }
+                if(this.x + 1 <= 8 && this.y + 2 <= 8)
+                {
+                    movesPos.push(this.x + 1);
+                    movesPos.push(this.y + 2);
+                }
+                break;
+            case 5: //Pawn moveset
+                if(this.isWhite) {
+                    movesPos.push(this.x);
+                    movesPos.push(this.y - 1);
+                    if(this.isfirstMove) {
+                        movesPos.push(this.x);
+                        movesPos.push(this.y - 2);
+                    }
+                } else {
+                    movesPos.push(this.x);
+                    movesPos.push(this.y + 1);
+                    if(this.isfirstMove) {
+                        movesPos.push(this.x);
+                        movesPos.push(this.y + 2);
+                    }
+                }
+                break;
+            default:
+                console.log("p_type out of bounds");
+                break;
+        }
+
+        return movesPos;
     }
 }
--- no_html/javascript/chess_board_no_html.js
+++ no_html/javascript/chess_board_no_html.js
@@ -7,13 +7,12 @@ function mainFunc() {
 
     pieces = []; //array that contains all the game pieces
     placePieces(); //adds the pieces to the array
-    selected = [0, 0]; //reserved for selected td
 
     for (let i = 0; i < 8; i++) {
         const tr = table.insertRow();
         for (let j = 0; j < 8; j++) {
             const td = tr.insertCell();
-            td.onclick = () => {clickedTD(td, i, j)};
+            td.onclick = () => {clickedTD(td, j, i)};
             if ((j + i) % 2 != 0) {
                 td.className = "lightSquare"
             } else {
@@ -28,59 +27,74 @@ function mainFunc() {
     });
 }
 
-function clickedTD(td, y, x) {
-    //color the last selected pixel
-    const tds = document.getElementsByTagName("td");
-    if ((selected[0] + selected[1]) % 2 != 0) {
-        tds[selected[1]*8 + selected[0]].style.backgroundColor = "#779556";
-    } else {
-        tds[selected[1]*8 + selected[0]].style.backgroundColor = "#ebecd0";
-    }
-    tds[selected[1]*8 + selected[0]].style.outline = "";
-    tds[selected[1]*8 + selected[0]].style.outlineOffset = "";
+function clickedTD(td, x, y) {
+    //repaint the whole board
+    repaintBoard();
+
     //color the current selected pixel
-    selected = [x, y];
     td.style.backgroundColor = 'orange';
     td.style.outline = "4px solid black";
     td.style.outlineOffset = "-4px";
 
-    //td.style.opacity = '0.7';
+    //paint possible moves for selected piece
+    showMoves(x, y);
+
+}
+
+function repaintBoard() {
+    const tds = document.getElementsByTagName("td");
+    for (let y = 0; y < 8; y++) {
+        for (let x = 0; x < 8; x++) {
+            if ((x + y) % 2 != 0) {
+                tds[y*8 + x].style.backgroundColor = "#779556";
+            } else {
+                tds[y*8 + x].style.backgroundColor = "#ebecd0";
+            }
+            tds[y*8 + x].style.outline = "";
+            tds[y*8 + x].style.outlineOffset = "";
+        }
+    }
+}
+
+function showMoves(x, y) {
+    let posMoves = [];
+    //get possible moves for piece
+    pieces.forEach(piece => {
+        if (piece.getArrPos() == (y*8+x)) {
+            posMoves = piece.possibleMoves();
+        }
+    });
+    //paint possible moves on board
+    const tds = document.getElementsByTagName("td");
+    for (let i = 0; i < posMoves.length; i+=2) {
+        // console.log("NOOt WOW: " + posMoves[i + 1] + " * 8 " + posMoves[i]);
+        // console.log("Much WOW SUM: " + Number(posMoves[i + 1] * 8 + posMoves[i]));
+        tds[posMoves[i + 1] * 8 + posMoves[i]].style.outline = "3px dashed gold";
+        tds[posMoves[i + 1] * 8 + posMoves[i]].style.outlineOffset = "-25px";
+    }
+
 }
 
 function placePieces() {
-    //black pieces
-    for (let i = 0; i < 16; i++) {
-        if(i == 0 || i == 7) {
-            pieces.push(new c_piece(i, 0, 8)); //rb
-        } else if(i == 1 || i == 6) {
-            pieces.push(new c_piece(i, 0, 10)); //nb
-        } else if(i == 2 || i == 5) {
-            pieces.push(new c_piece(i, 0, 9)); //bb
-        } else if(i == 3) {
-            pieces.push(new c_piece(i, 0, 7)); //qb
-        } else if(i == 4) {
-            pieces.push(new c_piece(i, 0, 6)); //kb
-        }
-        if(i > 7) {
-            pieces.push(new c_piece(i-8, 1, 11)); //pb
-        }
-    }
-
-    //white pieces
-    for (let i = 0; i < 16; i++) {
-        if(i == 0 || i == 7) {
-            pieces.push(new c_piece(i, 7, 2)); //rw
-        } else if(i == 1 || i == 6) {
-            pieces.push(new c_piece(i, 7, 4)); //nw
-        } else if(i == 2 || i == 5) {
-            pieces.push(new c_piece(i, 7, 3)); //bw
-        } else if(i == 3) {
-            pieces.push(new c_piece(i, 7, 1)); //qw
-        } else if(i == 4) {
-            pieces.push(new c_piece(i, 7, 0)); //kw
-        }
-        if(i > 7) {
-            pieces.push(new c_piece(i-8, 6, 5)); //pw
+    for (let i = 0; i < 2; i++) {
+        //i !== 0 is white(true) pieces so the line number would be able to be i * 7
+        for (let j = 0; j < 16; j++) {
+            if(j == 0 || j == 7) {
+                pieces.push(new c_piece(j, i * 7, 2, i !== 0, true)); //rook
+            } else if(j == 1 || j == 6) {
+                pieces.push(new c_piece(j, i * 7, 4, i !== 0, true)); //knight
+            } else if(j == 2 || j == 5) {
+                pieces.push(new c_piece(j, i * 7, 3, i !== 0, true)); //bishop
+            } else if(j == 3) {
+                pieces.push(new c_piece(j, i * 7, 1, i !== 0, true)); //queen
+            } else if(j == 4) {
+                pieces.push(new c_piece(j, i * 7, 0, i !== 0, true)); //king
+            }
+            if(j > 7 && i == 0) {
+                pieces.push(new c_piece(j-8, 1, 5, false, true)); //pawn black
+            } else if(j > 7 && i == 1) {
+                pieces.push(new c_piece(j-8, 6, 5, true, true)); //pawn white
+            }
         }
     }
 }
